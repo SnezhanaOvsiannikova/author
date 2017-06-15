c401e3d4862ea694ba5802d02d8996ab6d05bf7a
jack ovsiannikov <jack ovsiannikov>
2017-06-15 14:52:00 +0300

    add style

--- js/main.js
+++ js/main.js
@@ -1,9 +1,27 @@
-// $(document).ready(function(){
-//   (function() {
-//        setTimeout(function() {
-//               $('.imgScale').imageScale({
-//                      align: 'center'
-//               });
-//        }, 100);
-//   } ());	
-// });
+$(document).ready(function(){
+	$('#slider').slick({
+			cssEase: 'linear',
+			dots: false,
+			fade: true,
+			infinite: true,
+			slidesToScroll: 1,
+			slidesToShow: 1,
+			speed: 300,
+			swipeToSlide: true,
+			nextArrow: $('.banner button.right'),
+			prevArrow: $('.banner button.left'),
+		});
+  	$('.img-scale').imageScale({
+		rescaleOnResize: true,
+		align: 'center'
+	});
+});
+function validateCommentsFunc(textarea) {
+	 if (textarea.value.length < 20) {
+	  textarea.setCustomValidity("Give a more detailed answer, please...");   
+   }
+   else {
+	  textarea.setCustomValidity("");
+   }
+
+}
